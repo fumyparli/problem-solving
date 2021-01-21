5b673a6a2ceab3542fa8570da6d64198fe175dff
fumyparli <qkskskn@naver.com>
2021-01-21 20:41:16 +0900

    Add code

---
+++ pairAndRemove.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length !== 0 && stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    if (stack.length === 0) return 1;
+    return 0;
+}

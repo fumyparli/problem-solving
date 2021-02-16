defbc954add85a5cfae8d5d07fb8604d30b7b501
fumyparli <qkskskn@naver.com>
2021-02-16 16:19:44 +0900

    Add code

--- matchingPoint.cpp
+++ matchingPoint.cpp
@@ -10,6 +10,23 @@ struct info {
 
 map<string, info> m;
 
+string get_link(string s) {
+    int flag = 0;
+    string tmp = "";
+    for (int j = 0; j < s.size(); j++) {
+        if (flag == 1 && s[j] == '\"') {
+            break;
+        }
+        if (flag == 1) {
+            tmp += s[j];
+        }
+        if (flag == 0 && s[j] == '\"') {
+            flag = 1;
+        }
+    }
+    return tmp;
+}
+
 string to_upper(string s) {
     string ret = "";
     for (int i = 0; i < s.size(); i++) {
@@ -18,18 +35,12 @@ string to_upper(string s) {
     return ret;
 }
 
-bool cmp(pair<double, int>& a, pair<double, int>& b) {
-    if (a.first == b.first) {
-        return a.second < b.second;
-    }
-    return a.first > b.first;
-}
-
 int solution(string word, vector<string> pages) {
     string upper_word = to_upper(word);
     for (int i = 0; i < pages.size(); i++) {
         stringstream f(pages[i]);
         string current_link = "";
+        string prev = "";
         int meta_flag = 0;
         while (true) {
             string s;
@@ -37,32 +48,11 @@ int solution(string word, vector<string> pages) {
             if (s == "<meta") meta_flag = 1;
             if (meta_flag == 1 && s.substr(0, 7) == "content") {
                 meta_flag = 0;
-                int flag = 0;
-                string tmp = "";
-                for (int j = 0; j < s.size(); j++) {
-                    if (flag == 1 && s[j] == '\"') {
-                        break;
-                    }
-                    if (flag == 1) {
-                        tmp += s[j];
-                    }
-                    if (flag == 0 && s[j] == '\"') {
-                        flag = 1;
-                    }
-                }
+                string tmp = get_link(s);
                 m[tmp] = {i, 0, 0};
                 current_link = tmp;
             }
-        }
-        f.clear();
-        f.str(pages[i]);
-        string prev = "";
-        while (true) {
-            string s;
-            if (!(f >> s)) break;
-            int s_size = s.size();
-            int w_size = word.size();
-            if (s_size - w_size >= 0) {
+            if ((int)s.size() - (int)word.size() >= 0) {
                 for (int j = 0; j <= s.size() - word.size(); j++) {
                     if (to_upper(s.substr(j, word.size())) == upper_word && ((j == 0 || !isupper(toupper(s[j - 1]))) && (j == s.size() - word.size() || !isupper(toupper(s[j + word.size()]))))) {
                         m[current_link].def_point++;
@@ -71,18 +61,7 @@ int solution(string word, vector<string> pages) {
             }
             if (prev == "<a" && s.substr(0, 4) == "href") {
                 int flag = 0;
-                string tmp = "";
-                for (int j = 0; j < s.size(); j++) {
-                    if (flag == 1 && s[j] == '\"') {
-                        break;
-                    }
-                    if (flag == 1) {
-                        tmp += s[j];
-                    }
-                    if (flag == 0 && s[j] == '\"') {
-                        flag = 1;
-                    }
-                }
+                string tmp = get_link(s);
                 m[current_link].ext_link.push_back(tmp);
             }
             prev = "";
@@ -96,14 +75,14 @@ int solution(string word, vector<string> pages) {
     for (auto i = m.begin(); i != m.end(); i++) {
         for (auto& j : i->second.ext_link) {
             if (m.find(j) != m.end()) {
-                m[j].total_point += ((double)i->second.def_point / i->second.ext_link.size());
+                m[j].total_point -= ((double)i->second.def_point / i->second.ext_link.size());
             }
         }
     }
     for (auto i = m.begin(); i != m.end(); i++) {
-        i->second.total_point += i->second.def_point;
+        i->second.total_point -= i->second.def_point;
         ans.push_back({i->second.total_point, i->second.idx});
     }
-    sort(ans.begin(), ans.end(), cmp);
+    sort(ans.begin(), ans.end());
     return ans[0].second;
 }

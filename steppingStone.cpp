fee729e37eb5ee854b3d981cf0459ee6e772846c
fumyparli <qkskskn@naver.com>
2021-02-24 13:05:17 +0900

    Add code

--- steppingStone.cpp
+++ steppingStone.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define inf 2000000000
-int solution(vector<int> stones, int k) {
-    priority_queue<pair<int, int>> pq;
-    int min = inf;
-    for (int i = 0; i < k - 1; i++) pq.push({stones[i], i});
-    for (int i = 0; i <= stones.size() - k; i++) {
-        pq.push({stones[i + k - 1], i + k - 1});
-        while (!pq.empty()) {
-            if (!(i <= pq.top().second && pq.top().second <= i + k - 1))
-                pq.pop();
-            else
-                break;
-        }
-        if (min > pq.top().first) min = pq.top().first;
+
+vector<int> a;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int prev = 0;
+    sort(rocks.begin(), rocks.end());
+    for (int i = 0; i < rocks.size(); i++) {
+        a.push_back(rocks[i] - prev);
+        prev = rocks[i];
     }
-    return min;
+    a.push_back(distance - rocks[rocks.size() - 1]);
+    int st = 0;
+    int ed = distance;
+    while (true) {
+        int mid = (st + ed) / 2;
+        if (st >= ed) break;
+        int sum = 0;
+        int cnt = 0;
+        for (int i = 0; i < a.size(); i++) {
+            sum += a[i];
+            if (sum >= mid)
+                sum = 0;
+            else
+                cnt++;
+        }
+        if (cnt <= n)
+            st = mid + 1;
+        else
+            ed = mid;
+    }
+    return ed - 1;
 }

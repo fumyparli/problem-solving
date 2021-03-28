22ab04e62e959d3b767f9f01f66b52ee5f162128
fumyparli <qkskskn@naver.com>
2021-03-28 21:27:45 +0900

    Add code

---
+++ BACKJOON/13505_bit.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> a;
+
+struct trie {
+    struct node {
+        int child[2];
+        bool valid;
+        node() {
+            memset(child, -1, sizeof(child));
+            valid = false;
+        };
+    };
+    vector<node> tr;
+    int init() {
+        node new_node;
+        tr.push_back(new_node);
+        return (int)tr.size() - 1;
+    }
+    int root = init();
+    void insert(int x, int n, int i) {
+        if (i == -1) {
+            tr[x].valid = 1;
+            return;
+        }
+        int c = (n >> i) & 1;
+        if (tr[x].child[c] == -1) tr[x].child[c] = init();
+        int nx = tr[x].child[c];
+        insert(nx, n, i - 1);
+    }
+    void insert(int n) {
+        insert(root, n, 31);
+    }
+    int find(int x, int n, int i) {
+        if (i == -1) return 0;
+        int c = !((n >> i) & 1);
+        if (tr[x].child[c] == -1) c = !c;
+        int nx = tr[x].child[c];
+        return (c << i) | find(nx, n, i - 1);
+    }
+    int find(int n) {
+        return find(root, n, 31);
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    trie tr;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        a.push_back(k);
+        tr.insert(k);
+    }
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, a[i] ^ tr.find(a[i]));
+    }
+    cout << ans;
+}

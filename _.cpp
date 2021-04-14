03e6e2c7154b7b04874d0da2bed694b45a0bbd48
fumyparli <qkskskn@naver.com>
2021-04-14 23:47:42 +0900

    Add code

---
+++ _.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;  // 192457
+    int topick = n - k;
+    int i = 0;
+    string ans = "";
+    while (true) {
+        int maxv = -1, maxi = -1;
+        for (int j = i; j < n - topick + 1; j++) {
+            if (maxv < s[j]) {
+                maxv = s[j];
+                maxi = j;
+            }
+        }
+        ans += maxv;
+        i = maxi + 1;
+        topick--;
+        if (topick == 0) break;
+    }
+    cout << ans;
+}

8515298b821912e7718e3448cec1ce45bf227441
fumyparli <qkskskn@naver.com>
2021-05-07 20:13:23 +0900

    Add code

--- constructionRaceway.cpp
+++ constructionRaceway.cpp
@@ -1,46 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define inf 987654321
 
 int check[26][26];
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
-
-struct info {
-    int x;
-    int y;
-    int d;
-    int cost;
-};
-
 int solution(vector<vector<int>> a) {
-    for (auto& i : check) {
-        for (auto& j : i) {
-            j = inf;
-        }
-    }
-    queue<info> q;
-    q.push({0, 0, 0, 0});
-    check[0][0] = 0;
+    int n = a.size();
+    queue<tuple<int, int, int, int>> q;
+    q.push({0, 0, -1, 0});
+    check[0][0] = 1;
     while (!q.empty()) {
-        int x = q.front().x;
-        int y = q.front().y;
-        int d = q.front().d;
-        int cost = q.front().cost;
+        auto [x, y, d, cost] = q.front();
         q.pop();
         for (int k = 0; k < 4; k++) {
+            int ncost = cost + 100;
+            if (d != -1 && d != k) ncost += 500;
             int nx = x + dx[k];
             int ny = y + dy[k];
-            int ncost;
-            if ((x == 0 && y == 0) || d == k)
-                ncost = cost + 100;
-            else
-                ncost = cost + 600;
-            if (0 <= nx && nx < a.size() && 0 <= ny && ny < a.size() && check[nx][ny] >= ncost && a[nx][ny] == 0) {
-                check[nx][ny] = ncost;
-                q.push({nx, ny, k, ncost});
+            if (0 <= nx && nx < n && 0 <= ny && ny < n && a[nx][ny] == 0) {
+                if (check[nx][ny] == 0 || check[nx][ny] >= ncost) {
+                    check[nx][ny] = ncost;
+                    q.push({nx, ny, k, ncost});
+                }
             }
         }
     }
-    return check[a.size() - 1][a.size() - 1];
+    return check[n - 1][n - 1];
 }

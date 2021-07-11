8bddc46716e0eee152b849f1f8c4808e1d3655b7
fumyparli <qkskskn@naver.com>
2021-07-11 15:43:45 +0900

    Add code

---
+++ BACKJOON/15666.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> a;
+vector<int> list;
+void foo(int x) {
+    if (a.size() == m) {
+        for (int i = 0; i < m; i++) {
+            cout << a[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    int pre_num = -1;
+    for (int i = x; i < n; i++) {
+        if (pre_num != list[i]) {
+            a.push_back(list[i]);
+            foo(i);
+            a.pop_back();
+            pre_num = list[i];
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        list.push_back(k);
+    }
+    sort(list.begin(), list.end());
+    foo(0);
+}
